270b855b842c676a3d28847cb89f37a1de15cbe7
User <training-2022@mail.ru>
2022-07-19 14:29:57 +0300

    init

---
+++ src/main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+        webviewTag: true,
+        nodeIntegration: true,
+        contextIsolation: false,
+    }
+  });
+
+  mainWindow.loadFile(path.join(__dirname, 'app.html'));
+  //mainWindow.loadURL('site.com');
+};
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+
